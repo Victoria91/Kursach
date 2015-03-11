3574068ec56618368da26e4b7826013b607a6e50
Быкова Виктория Олеговна <vbykova@at-consulting.ru>
2015-03-11 13:13:33 +0300

    fix displaying of replacements if no medicine on storage

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,24 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-puts 'Creating forms...'
+puts 'Creating Forms...'
 forms = Form.first_or_create([
   { name: 'Капсулы' },
   { name: 'Таблетки' },
   { name: 'Пилюли' },
   { name: 'Раствор' }
 ])
+puts 'Creating Sales...'
 sales = Sale.first_or_create([
   { name: 'Фармамед ООО' },
   { name: 'ADVANCE PHARMA GMBH' },
   { name: 'Аквион ЗАО' }
+])
+puts 'Creating Medicines...'
+sales = Medicine.first_or_create([
+  { name: 'Глицин', min: 3, sale: Sale.first },
+  { name: 'Луцетам', min: 2, sale: Sale.first },
+  { name: 'Терафлю', min: 2, sale: Sale.last },
+  { name: 'Йодомарин', min: 10, sale: Sale.first },
+  { name: 'Пиковит', min: 5, sale: Sale.last }
 ])
